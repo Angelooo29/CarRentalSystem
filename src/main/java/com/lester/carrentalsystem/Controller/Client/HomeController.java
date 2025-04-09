0632a26300ff75668082e0c53f1cf5236cfa27a6
Katorino03 <kathleenmaefloresb@gmail.com>
2025-04-09 14:42:31 +0800

    gi merge na

---
+++ src/main/java/com/lester/carrentalsystem/Controller/Client/HomeController.java
@@ -0,0 +1,18 @@
+package com.lester.carrentalsystem.Controller.Client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    void onPreviousButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNextButtonClicked(ActionEvent event) {
+
+    }
+
+}
